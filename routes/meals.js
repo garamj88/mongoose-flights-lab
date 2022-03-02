0b869df9be8cc491824b707349d850aadbfbc165
Garam Jang <garamj88@gmail.com>
2022-03-02 18:49:13 -0500

    add create func for meals

--- routes/meals.js
+++ routes/meals.js
@@ -7,6 +7,9 @@ const router = Router()
 // GET - localhost:3000/meals/new
 router.get('/new', mealsCtrl.new)
 
+// POST - localhost:3000/meals
+router.post('/', mealsCtrl.create)
+
 export { 
   router
 }
